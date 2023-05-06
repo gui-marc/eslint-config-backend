539dae3a2d0c82e74beb376e7f6ee60588fd0308
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2023-05-06 14:23:47 +0100

    Added backend configs :sparkles:

---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  ignorePatterns: [".eslintrc.cjs", "vite.config.ts", "package.json"],
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "prettier", "eslint-plugin-import-helpers"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-throw-literal": "off", // use for handling app errors
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [
+          "module",
+          "/^@//",
+          ["parent", "sibling", "index"],
+        ],
+        alphabetize: {
+          order: "asc",
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,29 @@
 # Eslint Config
 
 This is a simple repo containing some eslint configs that I use in my projects.
+
+## Usage
+
+Install the package:
+
+```bash
+npm install --save-dev @gui-marc/eslint-config-backend
+```
+
+Then add the config to your eslint config file:
+
+```json
+{
+  "extends": "@gui-marc/eslint-config-backend"
+}
+```
+
+Then add this config to your `.prettierrc` file:
+
+```json
+{
+    "trailingComma": "none",
+    "semi": true,
+    "singleQuote": true
+}
+```
